c965253789c4d1bed27a76da0c6d2e981b70ae85
zhuxiaofeng <1063446979@qq.com>
2021-12-29 18:08:13 +0800

    Initial commit

---
+++ src/main/java/com/zxf/ApplicationStarter.java
@@ -0,0 +1,18 @@
+package com.zxf;
+
+import com.zxf.startup.BootStarter;
+import org.springframework.boot.SpringApplication;
+
+/**
+ * 项目启动类
+ *
+ * @author zhuxiaofeng
+ * @date 2021/12/29
+ */
+public class ApplicationStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootStarter.class, args);
+    }
+
+}
